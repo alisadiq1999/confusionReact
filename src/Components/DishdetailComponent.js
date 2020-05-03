a730dd99bb548ef6939ba8bb4c32b00f368bc50b
Ali Sadiq <msadiq.bscs17seecs@seecs.edu.pk>
2020-05-03 17:58:09 +0500

    Ass 1

---
+++ src/Components/DishdetailComponent.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
+
+class DishDetail extends Component{
+    constructor(props){
+        super(props);
+            this.state = {
+                
+            }
+        
+    }
+    renderDish(dish) {
+        if (dish != null)
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
+    renderComments(comment) {
+        
+        if (comment != null)
+            return(
+
+                <div>
+                    <h4>Comments</h4>
+                    {comment.comments.map((com) =>
+                    <ul key={com.id} className="list-unstyled"> 
+                        <li>{com.comment}</li>
+                        <li>-- {com.author}, {com.date}</li>
+                    </ul>
+                    )}
+                    
+                </div>
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
+    render(){
+
+        return(
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.props.selected)}
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderComments(this.props.selected)}
+                </div>
+                
+            </div>
+        );
+    }
+}
+
+export default DishDetail;
